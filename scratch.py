497754ad76ab81436e04ea1ac83a61fd7637c01f
saurabhmeshram2003 <your_email@gmail.com>
2026-04-26 03:01:06 +0530

    Initial commit - Alumni Project

--- scratch.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from extensions import mongo
-
-app = create_app()
-with app.app_context():
-    for member in mongo.db.users.find():
-        s = member.get('skills')
-        if not isinstance(s, list):
-            print(f"User {member['_id']} has invalid skills: {type(s)} -> {s}")
